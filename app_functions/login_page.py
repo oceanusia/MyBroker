d4840c9598a7d6acee47a5393cc4b279038cdae0
oceanusia <seinlee39@gmail.com>
2025-04-20 02:00:37 -0400

    Update login_page.py

--- app_functions/login_page.py
+++ app_functions/login_page.py
@@ -3,19 +3,22 @@ import time
 from db import authenticate_user
 
 # Login Function
-
 def login():
     st.title("Login")
+    
+    # Input fields
     email = st.text_input("University Email", key="login_email")
     password = st.text_input("Password", type="password", key="login_password")
 
+    # Login button
     if st.button("Login"):
+        # Authenticate against Supabase
         if authenticate_user(email, password):
             # Set logged-in state and store email
             st.session_state['logged_in'] = True
             st.session_state['verified_email'] = email
 
-            # Show a loading spinner and pause for 2 seconds
+            # Loading spinner for UX
             with st.spinner("Logging In..."):
                 time.sleep(2)
 
